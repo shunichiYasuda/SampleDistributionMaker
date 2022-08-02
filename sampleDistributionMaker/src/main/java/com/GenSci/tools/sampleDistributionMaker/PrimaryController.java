ba46d591f8edc007cc261c721c40d15026d4d8ab
Yasuda Shunichi <Yasuda Shunichi@DESKTOP-T2H829A>
2022-08-02 17:03:04 +0900

    Make Layout Aug 02

--- sampleDistributionMaker/src/main/java/com/GenSci/tools/sampleDistributionMaker/PrimaryController.java
+++ sampleDistributionMaker/src/main/java/com/GenSci/tools/sampleDistributionMaker/PrimaryController.java
@@ -2,11 +2,33 @@ package com.GenSci.tools.sampleDistributionMaker;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        //App.setRoot("secondary");
-    }
+    Button execBtn;
+    @FXML
+    Button quitBtn;
+    @FXML
+    TextField popSize; //母集団サイズ
+    @FXML
+    TextField popAve; //母集団平均値
+    @FXML
+    TextField popStdev; //母集団標準偏差
+    @FXML
+    TextField sampleSize; //標本サイズ
+    @FXML
+    TextField sampleNumber; //標本採取回数
+    @FXML
+    Label sampleAve; //標本平均の平均（標本平均）
+    @FXML
+    Label sampleStdev; //標本平均の標準偏差
+    @FXML
+    Label sampleUnVar; //標本平均の平均（標本平均）
+    @FXML
+    Label sampleStErr; //標準誤差
+    
 }
