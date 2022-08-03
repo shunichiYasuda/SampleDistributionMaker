658a54269e6829bacd525c43cf2c46927b52299a
Yasuda Shunichi <Yasuda Shunichi@DESKTOP-T2H829A>
2022-08-03 16:13:43 +0900

    add population distriburion type

--- sampleDistributionMaker/src/main/java/com/GenSci/tools/sampleDistributionMaker/PrimaryController.java
+++ sampleDistributionMaker/src/main/java/com/GenSci/tools/sampleDistributionMaker/PrimaryController.java
@@ -1,11 +1,16 @@
 package com.GenSci.tools.sampleDistributionMaker;
 
 import java.io.IOException;
+import java.util.Random;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class PrimaryController {
 
@@ -16,7 +21,13 @@ public class PrimaryController {
 	@FXML
 	Button quitBtn;
 	@FXML
-	TextField popSize; // 母集団サイズ
+	TextField popSizeNormal; // 母集団サイズ正規分布
+	@FXML
+	TextField popSizeUniform; //母集団サイズ一様分布
+	@FXML
+	TextField maxUniform; //一様分布データの最大値（サイコロで言えば「6」）
+	@FXML
+	TextField minUniform;//一様分布データの最小値（サイコロで言えば「1」）
 	@FXML
 	TextField popAve; // 母集団平均値
 	@FXML
@@ -33,8 +44,15 @@ public class PrimaryController {
 	Label sampleUnVar; // 標本平均の平均（標本平均）
 	@FXML
 	Label sampleStErr; // 標準誤差
-	//
-
+	@FXML
+	RadioButton rbt_Uniform;
+	@FXML
+	RadioButton rbt_Normal;
+	@FXML
+	ToggleGroup group;
+	
+	
+double[] pop ; //母集団データ
 	@FXML
 	private void quitAction() {
 		System.exit(0);
@@ -42,16 +60,18 @@ public class PrimaryController {
 
 	@FXML
 	private void execAction() {
-		double[] pop ; //母集団データ
 		// 母集団の作成
 		makePop();
+		
 	}
 
 	private void makePop() {
+		//乱数ジェネレータ
+		Random gen = new Random();
 		// パラメータの読み込み
 		int size = 0;
-		if (popSize.getText() != "") { // textField になにか書いてある
-			String txt = popSize.getText();
+		if (popSizeNormal.getText() != "") { // textField になにか書いてある
+			String txt = popSizeNormal.getText();
 			try {
 				size = Integer.parseInt(txt);
 			} catch (NumberFormatException e) {
@@ -80,7 +100,14 @@ public class PrimaryController {
 			}
 		}
 		//母集団データの生成
+		pop = new double[size];
+		//母集団のタイプが正規分布だった場合
+		if(rbt_Normal.isSelected()) {
+			for(int i=0;i<pop.length;i++) {
+				pop[i] = gen.nextGaussian();
+			}
 		
+		}
 	}
 
 }
